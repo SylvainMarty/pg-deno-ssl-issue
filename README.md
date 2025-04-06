a15652f1ad42a80dc3d6f2e2c5e125e77f1cdf74
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2025-04-06 18:18:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to use
+
+1. Install Deno 2 (I am currently using Deno 2.2.8)
+2. Run `deno task start`
---
+++ pg_bug.ts
@@ -0,0 +1,12 @@
+// @ts-types="npm:@types/pg@8.11.11"
+import pg from "npm:pg@8.14.1";
+
+const client = new pg.Client({
+  connectionString:
+    // CHANGE THIS TO YOUR OWN NEON DB
+    "postgresql://user:password@your-own-neon-domain.eu-central-1.aws.neon.tech/neondb?sslmode=require",
+});
+
+await client.connect();
+await client.query(`SELECT 1`);
+await client.end();
